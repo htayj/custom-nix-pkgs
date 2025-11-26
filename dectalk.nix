35907b7d124098d0a0ab51f610860790545d05ea
htayj <hardy.taylor.j@gmail.com>
2025-11-25 23:25:47 -0500

    fix dectalk

--- dectalk.nix
+++ dectalk.nix
@@ -11,12 +11,12 @@
 }:
 stdenv.mkDerivation {
   pname = "dectalk";
-  version = "2023-10-30";
+  version = "git";
   src = fetchFromGitHub {
     owner = "dectalk";
     repo = "dectalk";
     rev = "c3c1e4ae4117f951d8d7f9ef2848408ec79047fd";
-    sha256 = "sha256-CKWHx0jgWD5u1EfWlj6R901A+HvjsYsLg0M/Ny2f3R8=";
+    sha256 = "sha256-/Xk/X3j6poNG/13cvPUjda7LJu5BuWMT8/6BUSxcWZo=";
     leaveDotGit = true;
   };
   buildInputs = [
@@ -28,10 +28,17 @@ stdenv.mkDerivation {
     git
     pkg-config
   ];
-  setSourceRoot = "sourceRoot=$(echo */.)";
-  preConfigure = "cd src && ./autogen.sh";
-  preBuild = ''
-    mkdir -p ../dist/
-  '';
+  # setSourceRoot = "sourceRoot=$(echo */.)";
+  preConfigure = "mkdir dist && cd src && ./autogen.sh";
+  # configurePhase = "./configure";
 
+  makeFlags = [ "DESTDIR=$(out)" ];
+  installPhase = ''
+    mkdir -p $out/bin
+    ln $out/say $out/bin
+    ln $out/gspeak $out/bin
+    ln $out/dtmemory $out/bin
+    ln $out/aclock $out/bin
+    ln $out/windic $out/bin
+  '';
 }
